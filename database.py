33f17f0318457c182df664b81cd284ead0b06e07
revitalizeclinica <revitalizebancodedados@gmail.com>
2026-02-02 16:35:46 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,9 @@
-import os
 import psycopg2
 import streamlit as st
 
 def get_connection():
     try:
-        conn = psycopg2.connect(
-            host=st.secrets["SUPABASE_HOST"],
-            port=st.secrets["SUPABASE_PORT"],
-            database=st.secrets["SUPABASE_DB"],
-            user=st.secrets["SUPABASE_USER"],
-            password=st.secrets["SUPABASE_PASSWORD"]
-        )
+        conn = psycopg2.connect(st.secrets["DATABASE_URL"])
         return conn
     except Exception as e:
         st.error(f"Erro ao conectar ao banco: {e}")
