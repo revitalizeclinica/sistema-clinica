3a8800f03eb888e1d202fbf65eedf687db5a72bb
revitalizeclinica <revitalizebancodedados@gmail.com>
2026-02-02 18:48:47 -0300

    cria tela de cadastro

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 import streamlit as st
-from database import get_connection
+from database import get_connection, inserir_paciente
 
 st.title("Sistema Revitalize - Clínica")
 
-st.write("Teste de conexão com o banco de dados:")
+menu = st.sidebar.selectbox(
+    "Menu",
+    ["Início", "Cadastrar Paciente"]
+)
 
-conn = get_connection()
+if menu == "Início":
+    st.write("Bem-vindo ao sistema da clínica!")
+    conn = get_connection()
 
-if conn:
-    st.success("Conectado com sucesso ao Supabase!")
-    conn.close()
-else:
-    st.error("Falha na conexão com o banco.")
+    if conn:
+        st.success("Conectado com sucesso ao Supabase!")
+        conn.close()
+    else:
+        st.error("Falha na conexão com o banco.")
+
+elif menu == "Cadastrar Paciente":
+
+    st.subheader("Cadastro de Paciente")
+
+    with st.form("form_paciente"):
+
+        nome = st.text_input("Nome completo")
+        data_nascimento = st.date_input("Data de nascimento")
+        telefone = st.text_input("Telefone")
+        email = st.text_input("Email")
+        contato_emergencia = st.text_input("Contato de emergência")
+        observacoes = st.text_area("Observações")
+
+        enviado = st.form_submit_button("Salvar")
+
+        if enviado:
+            resultado = inserir_paciente(
+                nome,
+                data_nascimento,
+                telefone,
+                email,
+                contato_emergencia,
+                observacoes
+            )
+
+            if resultado is True:
+                st.success("Paciente cadastrado com sucesso!")
+            else:
+                st.error(f"Erro ao salvar: {resultado}")
