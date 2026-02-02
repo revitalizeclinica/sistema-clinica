f612c1892d069eb58bb4894b88fb89461439a238
revitalizeclinica <revitalizebancodedados@gmail.com>
2026-02-02 18:47:08 -0300

    Update database.py
    função para chamar o formulário

--- database.py
+++ database.py
@@ -8,3 +8,30 @@ def get_connection():
     except Exception as e:
         st.error(f"Erro ao conectar ao banco: {e}")
         return None
+
+def inserir_paciente(nome, data_nascimento, telefone, email, contato_emergencia, observacoes):
+    conn = get_connection()
+    if conn is None:
+        return False
+
+    try:
+        cur = conn.cursor()
+
+        sql = """
+        INSERT INTO paciente 
+        (nome, data_nascimento, telefone, email, contato_emergencia, observacoes)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+
+        cur.execute(sql, (nome, data_nascimento, telefone, email, contato_emergencia, observacoes))
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return True
+
+    except Exception as e:
+        conn.close()
+        return str(e)
+
